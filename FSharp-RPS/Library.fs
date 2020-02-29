8f21a6ba3db3468303413161579b9cfb0bac23fa
Johan Haleby <johan.haleby@gmail.com>
2020-02-29 14:22:08 +0100

    Initial commit

---
+++ FSharp-RPS/Library.fs
@@ -0,0 +1,123 @@
+﻿namespace RPS
+
+open System
+
+type GameId = Guid
+
+type PlayerId = Guid
+
+type Move =
+    | Rock
+    | Paper
+    | Scissors
+
+type PlayerMove =
+    { PlayerId: PlayerId
+      Move: Move }
+
+type GameResult =
+    | Won of PlayerId
+    | Tied
+
+type GameProgress =
+    | Uninitialized
+    | NotStarted
+    | FirstMoveMade of PlayerMove
+    | GameEnded of GameResult
+
+type GameState =
+    { GameId: GameId
+      GameProgress: GameProgress }
+
+// Commands
+type Command =
+    | CreateGame of GameId * PlayerId
+    | Play of GameId * PlayerId * Move
+
+// Events
+type DomainEvent =
+    abstract GameId: GameId
+
+type GameCreated =
+    { GameId: GameId
+      CreatedBy: PlayerId }
+    interface DomainEvent with
+        member x.GameId = x.GameId
+
+type MoveMade =
+    { GameId: GameId
+      PlayerId: PlayerId
+      Move: Move }
+    interface DomainEvent with
+        member x.GameId = x.GameId
+
+type GameWon =
+    { GameId: GameId
+      WinnerId: PlayerId }
+    interface DomainEvent with
+        member x.GameId = x.GameId
+
+type GameTied =
+    { GameId: GameId }
+    interface DomainEvent with
+        member x.GameId = x.GameId
+
+// Derive current state from previous events
+module internal RehydrateGameState =
+    let private apply (state: GameState) (event: DomainEvent) =
+        match event with
+        | :? GameCreated as g ->
+            { GameId = g.GameId
+              GameProgress = NotStarted }
+        | :? MoveMade as m ->
+            if (state.GameProgress = NotStarted) then
+                { state with GameProgress = FirstMoveMade { PlayerId = m.PlayerId; Move = m.Move } }
+            else
+                state
+        | _ -> state
+
+    let rehuydrate (events: DomainEvent list): GameState =
+        let initialState =
+            { GameId = GameId.NewGuid()
+              GameProgress = Uninitialized }
+        List.fold apply initialState events
+
+// The actual game
+module Game =
+    let private createGame state gameId createdBy: DomainEvent list =
+        match state.GameProgress with
+        | Uninitialized ->
+            let gameCreated =
+                { GameId = gameId
+                  CreatedBy = createdBy }
+            [ gameCreated ]
+        | _ -> []
+
+    // Simulate infix function with extension function....
+    type private Move with
+        member m1.beats (m2: Move) =
+            match (m1, m2) with
+            | (Rock, Scissors) -> true
+            | (Paper, Rock) -> true
+            | (Scissors, Paper) -> true
+            | _ -> false
+
+    let private play state playerId move: DomainEvent list =
+        match state.GameProgress with
+        | NotStarted ->
+            [ { MoveMade.GameId = state.GameId; PlayerId = playerId; Move = move } ]
+        | FirstMoveMade {PlayerId=player1; Move=firstMove}  ->
+            let moveMadeEvent = { MoveMade.GameId = state.GameId; PlayerId = playerId; Move = move }
+            let gameResultEvent = if firstMove = move then
+                                    { GameTied.GameId = state.GameId } :> DomainEvent
+                                  elif firstMove .beats move then
+                                    { GameWon.GameId = state.GameId; WinnerId = player1} :> DomainEvent
+                                  else
+                                    { GameWon.GameId = state.GameId; WinnerId = playerId} :> DomainEvent
+            [moveMadeEvent; gameResultEvent]
+        | _ -> []
+
+    let handle (state: GameState) (cmd: Command): DomainEvent list =
+        match cmd with
+        | CreateGame(gameId, createdBy) -> createGame state gameId createdBy
+        | Play(_, playerId, move) -> play state playerId move
