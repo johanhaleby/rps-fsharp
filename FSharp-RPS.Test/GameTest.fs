7946918c20933945be32427df26c0c8a6db05689
Johan Haleby <johan.haleby@gmail.com>
2020-03-01 13:06:55 +0100

    Actually including tests

---
+++ FSharp-RPS.Test/GameTest.fs
@@ -0,0 +1,79 @@
+module Tests
+
+open NFluent
+open Xunit
+open RPS
+open RehydrateGameState
+open Xunit
+
+[<Fact>]
+let ``Returns game created event with supplied game id and creator when creating new game``() =
+    // Given
+    let state: GameState =
+        { GameId = GameId.NewGuid()
+          GameProgress = Uninitialized }
+    let gameId = GameId.NewGuid()
+    let playerId = PlayerId.NewGuid()
+    let cmd : Command = CreateGame(gameId, playerId)
+    
+    // When
+    let events : DomainEvent list = Game.handle state cmd
+    
+    // Then
+    let expected : DomainEvent list = [{GameId = gameId; CreatedBy = playerId }]
+    Assert.Equal<DomainEvent list>(expected, events)
+    
+[<Fact>]
+let ``Returns move made event with supplied game id, player id and move player is making the first move of the game``() =
+    // Given
+    let gameId = GameId.NewGuid()
+    let playerId = PlayerId.NewGuid()
+    let previousEvents : DomainEvent list = [{GameCreated.GameId = gameId; CreatedBy = playerId }]
+    
+    let state = rehydrateGameStateFrom previousEvents 
+    let cmd = Play(gameId, playerId, move = Rock)
+    
+    // When
+    let events : DomainEvent list = Game.handle state cmd
+    
+    // Then
+    let expected : DomainEvent list =  [{MoveMade.GameId = gameId; PlayerId = playerId; Move = Rock }]
+    Assert.Equal<DomainEvent list>(expected, events)
+    
+[<Fact>]
+let ``Returns move made and game won events when second player beats first player``() =
+    // Given
+    let gameId = GameId.NewGuid()
+    let firstPlayerId = PlayerId.NewGuid()
+    let secondPlayerId = PlayerId.NewGuid()
+    let previousEvents : DomainEvent list = [{GameCreated.GameId = gameId; CreatedBy = firstPlayerId }
+                                             {MoveMade.GameId = gameId; PlayerId = firstPlayerId; Move = Rock }]
+    
+    let state = rehydrateGameStateFrom previousEvents 
+    let cmd = Play(gameId, secondPlayerId, move = Paper)
+    
+    // When
+    let events : DomainEvent seq = Game.handle state cmd |> Seq.ofList
+    
+    // Then
+    Check.That(events).Contains({GameWon.GameId = gameId; WinnerId = secondPlayerId},
+                                {MoveMade.GameId = gameId; PlayerId = secondPlayerId; Move = Paper })
+    
+[<Fact>]
+let ``Returns move made and game tied events when second player makes same move as first player``() =
+    // Given
+    let gameId = GameId.NewGuid()
+    let firstPlayerId = PlayerId.NewGuid()
+    let secondPlayerId = PlayerId.NewGuid()
+    let previousEvents : DomainEvent list = [{GameCreated.GameId = gameId; CreatedBy = firstPlayerId }
+                                             {MoveMade.GameId = gameId; PlayerId = firstPlayerId; Move = Scissors }]
+    
+    let state = rehydrateGameStateFrom previousEvents 
+    let cmd = Play(gameId, secondPlayerId, move = Scissors)
+    
+    // When
+    let events : DomainEvent seq = Game.handle state cmd |> Seq.ofList
+    
+    // Then
+    Check.That(events).Contains({GameTied.GameId = gameId},
+                                {MoveMade.GameId = gameId; PlayerId = secondPlayerId; Move = Scissors })
